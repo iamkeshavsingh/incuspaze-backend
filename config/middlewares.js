a17b1454e266330f0ba5236ad4ce4e1303f61694
Keshav Singh <keshavsingh@Keshavs-Mac-mini.local>
2026-06-14 14:54:13 +0530

    monsoon apis added

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,17 @@ module.exports = [
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
-  "strapi::body",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "40mb",
+      jsonLimit: "40mb",
+      textLimit: "40mb",
+      formidable: {
+        maxFileSize: 40 * 1024 * 1024,
+      },
+    },
+  },
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
